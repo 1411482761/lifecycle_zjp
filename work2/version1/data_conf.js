0a99291690d0a217b8bdb187bf3bf04aded9c827
ZhangJianping <zhang.jianping@lifecycle.cn>
2018-04-18 19:09:47 +0800

    完善版数据格式

--- work2/version1/data_conf.js
+++ work2/version1/data_conf.js
@@ -1,11 +1,45 @@
-{"msg":[
-    {"name":"name","describe":"姓名","type":"text"},
-    {"name":"phone","describe":"手机号","type":"text"},
-    {"name":"pic","describe":"二维码","type":"img"}],
-    "schedule":[
-        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"},
-        {"date":"5月1日","time":"9:00~10:00","event":"开会","address":"明德楼","detail":"二楼301室"},
-        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"},
-        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"}
+const data = {
+    "column": conf,
+    "participant": {
+        "id": "0212151105",
+        "url": "https://www.imooc.com/static/img/index/logo.png",
+        "name": "张三",
+        "phone": "1300000000",
+        "class": "opt1",
+        "pic": "https://www.imooc.com/static/img/index/logo.png"
+    },
+    "schedule": [
+        {
+            "date": "2019-05-01",
+            "time": "08:00",
+            "duration": "8:00~9:00",
+            "event": "吃早饭",
+            "address": "餐厅",
+            "detail": "二楼"
+        },
+        {
+            "date": "2019-05-01",
+            "time": "08:00",
+            "duration": "9:00~10:00",
+            "event": "开会",
+            "address": "明德楼",
+            "detail": "二楼301室"
+        },
+        {
+            "date": "2019-05-02",
+            "time": "08:00",
+            "duration": "8:00~9:00",
+            "event": "吃早饭",
+            "address": "餐厅",
+            "detail": "二楼"
+        },
+        {
+            "date": "2019-05-02",
+            "time": "08:00",
+            "duration": "9:00~10:00",
+            "event": "吃早饭",
+            "address": "餐厅",
+            "detail": "二楼"
+        }
     ]
-}
+};
--- work2/version1/face_onf.js
+++ work2/version1/face_onf.js
@@ -1,5 +1,18 @@
-const face={"msg":[
-    {"name":"name","describe":"姓名","type":"text"},
-    {"name":"phone","describe":"手机号","type":"text"},
-    {"name":"pic","describe":"照片","type":"file"}
-]}
+const conf = {
+    "column": [
+        {"name": "name", "describe": "姓名", "type": "text", "is_required": true},
+        {"name": "age", "describe": "年龄", "type": "text", "is_required": false, "is_enabled": false},
+        {"name": "phone", "describe": "手机号", "type": "text"},
+        {"name": "pic", "describe": "照片", "type": "file"},
+        {
+            "name": "class",
+            "describe": "课堂",
+            "type": "select",
+            "options": [
+                {"describe": "选项1", "value": "opt1"},
+                {"describe": "选项2", "value": "opt2"},
+                {"describe": "选项3", "value": "opt3"}
+            ]
+        }
+    ]
+};
