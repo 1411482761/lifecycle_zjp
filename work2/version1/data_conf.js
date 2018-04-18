e9e32ad04ee44d871a71954b1df7203f7a96f054
ZhangJianping <zhang.jianping@lifecycle.cn>
2018-04-18 18:39:11 +0800

    初步设计的数据格式

---
+++ work2/version1/data_conf.js
@@ -0,0 +1,11 @@
+{"msg":[
+    {"name":"name","describe":"姓名","type":"text"},
+    {"name":"phone","describe":"手机号","type":"text"},
+    {"name":"pic","describe":"二维码","type":"img"}],
+    "schedule":[
+        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"},
+        {"date":"5月1日","time":"9:00~10:00","event":"开会","address":"明德楼","detail":"二楼301室"},
+        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"},
+        {"date":"5月1日","time":"8:00~9:00","event":"吃早饭","address":"餐厅","detail":"二楼"}
+    ]
+}
---
+++ work2/version1/face_onf.js
@@ -0,0 +1,5 @@
+const face={"msg":[
+    {"name":"name","describe":"姓名","type":"text"},
+    {"name":"phone","describe":"手机号","type":"text"},
+    {"name":"pic","describe":"照片","type":"file"}
+]}
