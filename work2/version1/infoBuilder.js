8e3f693dbb0f315df991bae7fd886001f31b5164
ZhangJianping <zhang.jianping@lifecycle.cn>
2018-04-20 18:17:30 +0800

    时间差加上

--- work2/version1/infoBuilder.js
+++ work2/version1/infoBuilder.js
@@ -20,21 +20,50 @@ $(function(){
         var target=event.target;
         var index=target.name;//数组下标
         var arr=data.schedule;
-        var date=new Date(arr[index]["date"]);
+        var scheduleDate=arr[index]["date"];
+        var date=new Date(scheduleDate);
+        var hour=arr[index]["time"];
+        var datetime=scheduleDate+" "+hour+":00";
+        var myTime=new Date(datetime);
+        var myTimestamp=Date.parse(myTime);//获取日程时间戳
+        //var nowdate=new Date();
+        var nowTimestamp=Date.parse(new Date());
+        var leftDay=Math.floor((myTimestamp-nowTimestamp)/1000/60/60/24);
+        var leftHours=Math.floor((myTimestamp-nowTimestamp)/1000/60/60-24*leftDay);
+        var leftminutes=Math.floor((myTimestamp-nowTimestamp)/1000/60-24*leftDay*60-leftHours*60);
+        alert(typeof(leftminutes))
+        alert(leftminutes)
+        if(myTimestamp<nowTimestamp){
+            leftDay=0;
+            leftHours=0;
+            leftminutes=0;
+        }
+        //alert(leftDay+"天"+leftHours+"hours"+leftminutes)
         var month=date.getMonth()+1;
         var day=date.getDate();
-        var str='<form method="post"><input type="hidden" name="id" value="'+data.participant["id"]+'"><h3>'+month+'月'+day+'日</h3>&nbsp&nbsp&nbsp&nbsp&nbsp'+arr[index]["duration"]+'&nbsp&nbsp&nbsp&nbsp'+arr[index]["event"]+'<br/>' +
-            '&nbsp&nbsp&nbsp&nbsp&nbsp'+arr[index]["address"]+'<br/>&nbsp&nbsp&nbsp&nbsp&nbsp'+arr[index]["detail"]+'<br><br>' +
-            '<span>已签到<strong>'+3+'</strong>次</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="submit" value="签到" /><input type="button" name="exit" value="退出" ></form> ';
-        popDiv.append($(str));
+        var tempForm=$('<form method="post"><input type="hidden" name="id" value="'+data.participant["id"]+'"></form>');
+        var tempDate=$('<h3>'+month+'月'+day+'日</h3>');
+        var tempUl=$('<ul style="list-style:none"><li>'+arr[index]["duration"]+'</li><li>'+arr[index]["event"]+'</li><li>' +
+        arr[index]["address"]+'</li><li>'+arr[index]["detail"]+'</li></ul>');
+        var tempSign=$('<span>已签到<strong>'+3+'</strong>次</span>');
+        var tempSubmit=$('<input type="submit" value="签到" />');
+        var tempExit=$('<input type="button" name="exit" value="退出" >');
+        var timeleft=$('<span>距离本日程开始还有'+leftDay+'天'+leftHours+'小时'+leftminutes+'分钟</span>');
+        tempForm.append(tempDate).append(tempUl).append(tempSign).append(timeleft).append(tempSubmit).append(tempExit);
+        popDiv.append(tempForm);
+       /* var str='<form method="post"><input type="hidden" name="id" value="'+data.participant["id"]+'"><h3>'+month+'月'+day+'日</h3><ul style="list-style:none"><li>'+arr[index]["duration"]+'</li><li>'+arr[index]["event"]+'</li><li>' +
+            arr[index]["address"]+'</li><li>'+arr[index]["detail"]+'</li></ul>' +
+            '<span>已签到<strong>'+3+'</strong>次</span><br/>' +
+            '<input type="submit" value="签到" /><input type="button" name="exit" value="退出" ></form> ';
+        popDiv.append($(str));*/
         popDiv.show(300);
     });
     //生成参与者个人信息展示
    function _buildinfo() {
        var dom=$("<div></div>");
        var str='<img src="'+data.participant["url"]+'" /><br/>'+
-                '姓名:&nbsp&nbsp&nbsp&nbsp<em>'+data.participant["name"]+'</em><br/>'+
-                '手机号:&nbsp&nbsp&nbsp&nbsp'+data.participant["phone"]+'<br/>';
+                '姓名:<em>'+data.participant["name"]+'</em><br/>'+
+                '手机号:'+data.participant["phone"]+'<br/>';
        dom.append($(str));
        return dom;
    }
@@ -53,16 +82,15 @@ $(function(){
                var month=date.getMonth()+1;
                var day= date.getDate();
                dom.append($('<h4>'+month+'月'+day+'日</h4>'));
+               dom.append($('<ul></ul>'));
            }
-           var str='<h7>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="javascript:void(0)" data-index="index" name="'+index+'">'+arr[i]["duration"]+'&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+arr[i]["event"]+'</a></h7><br/>';
+           var str='<li><a href="javascript:void(0)" data-index="index" name="'+index+'">'+arr[i]["duration"]+arr[i]["event"]+'</a></li>';
            index++;
-           dom.append($(str));
+           dom.children("ul:last-child").append($(str));
            domArr.push(dom);
            scheduleDiv.append(dom);
        }
 
    }
-   function showDetails() {
-       
-   }
+
 })
